e7d5a305fcaea555a79870cea20146f9a2eda8c6
ademyrodev <ademerebaixadp@gmail.com>
2023-09-23 13:22:14 +0200

    update tests

--- tests/test.c
+++ tests/test.c
@@ -1,18 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "datastructures/dynarr.c"
-
-void printint(void *ptr) {
-    int *i = (int *) ptr;
-    printf("%d", *i);
-}
-
 int main(void) {
-    DynArr *dynarr = dynarr_fromrange(0, 10);
-    dynarr_print(dynarr, printint);
-
-    DynArr *dynarrcopy = dynarr_cpy(dynarr);
-    dynarr_print(dynarrcopy, printint);
-
-    dynarr_free(dynarr, free);
+    // placeholder (!)
 }
